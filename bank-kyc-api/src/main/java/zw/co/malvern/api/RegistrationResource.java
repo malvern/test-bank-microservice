4f7117eb6ee655b0a321bc8330eadeecf36574b5
malvern <malverndongeni@gmail.com>
2021-06-01 22:09:27 +0200

    Dockerfile file: #fix 4  - created dockerfile for bank-gateway-server  - docker-compose file has been updated

--- bank-kyc-api/src/main/java/zw/co/malvern/api/RegistrationResource.java
+++ bank-kyc-api/src/main/java/zw/co/malvern/api/RegistrationResource.java
@@ -2,13 +2,18 @@ package zw.co.malvern.api;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
 @CrossOrigin
 @RestController
 @RequestMapping("api/kyc")
 public class RegistrationResource {
 
     @PostMapping("account/create")
-    public String createCustomerAccount(@RequestBody CustomerRequest customerRequest){
-        return " Account for "+ customerRequest.getName()+" was successfully created";
+    public String createCustomerAccount(@RequestBody CustomerRequest customerRequest) {
+        return " Account for " + customerRequest.getName() + " was successfully created";
     }
 }
